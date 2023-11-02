376f4142c73e776ea2d7c986b1dead6575243fc3
Nikita Lukyanchuk <nikluk@mail.ru>
2023-11-02 23:49:31 +0300

    create workflow

--- quiz/quiz/wsgi.py
+++ quiz/quiz/wsgi.py
@@ -7,9 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
+
+import os
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quiz.settings')
 
